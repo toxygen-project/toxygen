b9446235ca93db9fa90b2034a60a764573208245
ingvar1995 <IgorN1995@yandex.ru>
2016-02-17 22:04:39 +0300

    defaults settings added

--- src/settings.py
+++ src/settings.py
@@ -7,9 +7,35 @@ class Settings(object):
 
     def __init__(self):
         self.path = Settings.get_default_path() + 'toxygen.json'
-        with open(self.path) as fl:
-            data = fl.read()
-        self.data = json.loads(data)
+        # TODO: create new if old not found
+        if os.path.exists(self.path):
+            with open(self.path) as fl:
+                data = fl.read()
+            self.data = json.loads(data)
+        else:
+            self.create_default_settings()
+
+    def create_default_settings(self):
+        self.data = {
+            "theme": "default",
+            "ipv6_enabled": True,
+            "udp_enabled": True,
+            "proxy_type": 0,
+            "proxy_host": "0",
+            "proxy_port": 0,
+            "start_port": 0,
+            "end_port": 0,
+            "tcp_port": 0,
+            "notifications": True,
+            "sound_notifications": False,
+            "language": "en-en",
+            "save_history": False,
+            "allow_inline": True,
+            "allow_auto_accept": False,
+            "auto_accept_from_friends": [],
+            "friends_aliases": [],
+            "typing_notifications": True
+        }
 
     def __get__(self, attr):
         return self.data[attr]
