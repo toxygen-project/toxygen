0f6984c63e46e6eb3fe483bc0bdb56fb7463874b
Андрей Владимирович <xveduk@tut.by>
2016-02-27 19:28:46 +0300

    del converting from util

--- src/util.py
+++ src/util.py
@@ -9,15 +9,6 @@ def log(data):
         fl.write(str(data))
 
 
-def string_to_bin(tox_id):
-    return tox_id.decode('hex')
-
-
-def bin_to_string(raw_id):
-    res = ''.join('{:02x}'.format(ord(x)) for x in raw_id)
-    return res.upper()
-
-
 def curr_directory():
     return os.path.dirname(os.path.realpath(__file__))
 
